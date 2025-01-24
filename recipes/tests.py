499c37339bb4363f6f4971100b981d106d01a665
LucianoCLopes <luciano_clopes@hotmail.com>
2025-01-24 13:17:37 -0300

    TEST - add config test and first test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from recipes import views
+
+class RecipeHomeViewTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func.view_class, views.HomeView)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
